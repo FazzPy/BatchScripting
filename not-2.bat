50310cde7f4f8c24a89eaad5afc396e63dea2c70
Fazz <97360909+FazzPy@users.noreply.github.com>
2022-07-31 15:51:10 +0300

    Update not-2.bat

--- not-2.bat
+++ not-2.bat
@@ -37,4 +37,115 @@ set /p Mail:Mail:
 echo Your Mail : %Mail%
 
 ::Fonksiyonlar::
+@echo off
+goto :main
 
+:say_something
+	echo I am saying %~1 and %~2!
+goto :eof
+
+:main
+	echo This is the main function!
+
+	call :say_something tiger boat
+	echo End
+goto :eof
+
+goto :main2
+
+:new_function
+	echo        Changing a variable...
+	set variable=VALUE
+goto :eof
+
+
+:main2
+	echo This is the main function
+	call :new_function
+	echo %variable%
+	echo The variables new value is %variable%
+goto :eof
+
+:local_function
+setlocal
+	echo This is Local Variable
+	set local_var=5
+endlocal
+goto eof
+
+::İf - Else::
+@echo off
+
+goto :main
+:main
+setlocal
+	echo Hello World!
+	echo .
+
+	set /a food=10
+	set /a needed_food=10
+
+	if %food% equ %needed_food% (
+		echo We have enough food!
+	)else(
+		echo We do not have enough food!
+	)
+
+	::equ = Eşit | neq = Eşit değil | lss = 'den daha az | leq = daha az veya eşit | gtr = 'den daha büyük | geq = daha büyük veya eşit::
+
+	echo .
+	echo Goodbye World!
+endlocal
+
+goto :eof
+
+::While Loop::
+@echo off
+setlocal EnableDelayedExpansion
+goto:main
+
+
+
+:main
+setlocal
+
+set /a counter=0
+set /a limit=10
+
+:loop
+if !counter! lss !limit! (
+
+	echo !counter!
+	set /a counter=!counter!+1
+
+	goto :loop
+)
+
+echo .
+echo Outside of loop^^!
+endlocal
+
+::For Loop::
+@echo off
+
+setlocal EnableDelayedExpansion
+goto :main
+
+:main
+setlocal
+	
+	set string=Hello World^^!
+	echo !string!
+	echo.
+
+	set /a test=3
+
+	for %%g in ( 1 2 3 4 5 ) do (
+		echo %%g
+		if !test! equ %%g (
+			echo !test! is %%g
+		)
+	)
+
+endlocal
+goto :eof
