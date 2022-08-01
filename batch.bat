0c68ee371cafb260bec435d4f6815d3750bda38c
Fazz <97360909+FazzPy@users.noreply.github.com>
2022-08-01 23:30:20 +0300

    Update batch.bat

--- batch.bat
+++ batch.bat
@@ -281,3 +281,33 @@ setlocal
 
 endlocal
 goto :eof
+
+::Registry::
+
+@echo off
+
+REM SYNTAX
+REM REG ADD [ROOT\]RegKey /v ValueName [/t DataType] [/S Separator] [/d Data] [/f]
+REM REG ADD [ROOT\]RegKey /ve [/d Data] [/f]
+
+REG ADD HKEY_CURRENT_USER\Console /v DataDemo /d "This is a entry"
+REG QUERY HKEY_CURRENT_USER\Console /v DataDemo
+
+::Processes::
+
+@echo off 
+REM To get the list of all the running processes
+TASKLIST
+REM Logging in a file
+TASKLIST > Processes.txt
+REM Tell you the memory usage greater tha a number
+REM Here /fi displays a set of tasks that match a given criteria specified by the filter
+tasklist /fi "memusage gt 50000" > ProcessesGT50000.txt
+
+REM Killing a process
+REM Syntax Taskkill /f /im name.exe
+
+REM Starting a new process
+REM Syntax START "title" [/D path] [options] "command" [parameters]
+
+START notepad.exe "notepadfile.txt"
